90c3aa0e038c2fc26a7c6e6a85b010391f5ce379
Libor Vašíček <libor.vasicek@braiins.cz>
2023-05-04 16:48:47 +0200

    ii1-stm: Add new linux target stm32mp1 #BOS-1080
    - add 'Makefile' for initial implementation of target 'stm32mp1' for
      Braiins control board
    - add 'config-5.10' with default kernel configuration
    - add 'image/Makefile' with dummy image

---
+++ target/linux/stm32mp1/ii1_stm/target.mk
@@ -0,0 +1,9 @@
+# SPDX-License-Identifier: GPL-2.0-only
+#
+# Copyright (C) 2023  Braiins Systems s.r.o.
+
+include $(TOPDIR)/rules.mk
+
+BOARDNAME:=Braiins Control Board v1 (STM32MP1)
+CPU_TYPE:=cortex-a7
+CPU_SUBTYPE:=neon-vfpv4
