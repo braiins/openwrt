99bcaa7d6478786c1c70e6cc58e22f56ae189d14
Libor Vašíček <libor.vasicek@braiins.cz>
2023-07-22 13:25:04 +0200

    stm32mp15_ii1: Generate /etc/bos_platform #BOS-1387
    - add 'BOARD_NAME_PATH', 'BOS_PLATFORM_PATH' variables with paths to
      specified config to 'bos-defaults.sh'
    - move all variables with paths to one sorted block at the beginning of
      'bos-defaults.sh'
    - add dynamic variable 'BOARD_NAME' to 'bos-defaults.sh'
    - add function '_config_bos_platform' to '91_default_config' to generate
      or update '/etc/bos_platform' using 'BOARD_NAME'

--- target/linux/stm32mp15/base-files/lib/functions/bos-defaults.sh
+++ target/linux/stm32mp15/base-files/lib/functions/bos-defaults.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
+BOARD_NAME_PATH="/tmp/sysinfo/board_name"
+BOS_PLATFORM_PATH="/etc/bos_platform"
+MINER_HWID_PATH="/tmp/miner_hwid"
+NVMEM_PATH="/sys/bus/nvmem/devices/stm32-romem0/nvmem"
+
+BOARD_NAME=$(cat "$BOARD_NAME_PATH")
+
 PART_NAME_FIP="fip"
 
-NVMEM_PATH="/sys/bus/nvmem/devices/stm32-romem0/nvmem"
 NVMEM_WORD_SIZE=4
 NVMEM_OTP_MINER_HWID=63
-
-MINER_HWID_PATH="/tmp/miner_hwid"
