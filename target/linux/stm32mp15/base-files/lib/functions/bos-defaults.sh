5e474a9f8c75ba2ea0b60153054bef1dba74b3ec
Libor Vašíček <libor.vasicek@braiins.cz>
2023-07-22 20:16:03 +0200

    stm32mp15_ii1: Generate /etc/bos_mode #BOS-1395
    - add variable 'BOS_MODE_PATH' to 'bos-defaults.sh' with path to config
    - add variable 'BRAIINS_BOARD_stm32mp157c_ii1_am2' with name of DTS
      compatible for specific Braiins board
    - add function '_config_bos_mode' to '91_default_config' to generate
      or update '/etc/bos_mode' using kernel argument 'root='

--- target/linux/stm32mp15/base-files/lib/functions/bos-defaults.sh
+++ target/linux/stm32mp15/base-files/lib/functions/bos-defaults.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
 BOARD_NAME_PATH="/tmp/sysinfo/board_name"
+BOS_MODE_PATH="/etc/bos_mode"
 BOS_PLATFORM_PATH="/etc/bos_platform"
 MINER_HWID_PATH="/tmp/miner_hwid"
 NVMEM_PATH="/sys/bus/nvmem/devices/stm32-romem0/nvmem"
 
 BOARD_NAME=$(cat "$BOARD_NAME_PATH")
+
+BRAIINS_BOARD_stm32mp157c_ii1_am2="braiins,stm32mp157c-ii1-am2"
 
 PART_NAME_FIP="fip"
 
