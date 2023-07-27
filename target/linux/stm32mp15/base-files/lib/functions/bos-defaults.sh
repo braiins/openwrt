ea0f5aad46da541e519cf3742a93b728ce9d3840
Libor Vašíček <libor.vasicek@braiins.cz>
2023-07-27 18:35:27 +0200

    stm32mp15_ii1: Improve bos-defaults.sh #BOS-1389
    - remove variable 'BOARD_NAME' and use standard function '$(board_name)'
      instead
    - replace direct use of 'BOS_MODE_PATH' with function '$(bos_mode)'
    - add variables with paths to all important BOS files and add equivalent
      function for getting its value

--- target/linux/stm32mp15/base-files/lib/functions/bos-defaults.sh
+++ target/linux/stm32mp15/base-files/lib/functions/bos-defaults.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-BOARD_NAME_PATH="/tmp/sysinfo/board_name"
+BOS_MAJOR_PATH="/etc/bos_major"
 BOS_MODE_PATH="/etc/bos_mode"
 BOS_PLATFORM_PATH="/etc/bos_platform"
+BOS_VERSION_PATH="/etc/bos_version"
 MINER_HWID_PATH="/tmp/miner_hwid"
 NVMEM_PATH="/sys/bus/nvmem/devices/stm32-romem0/nvmem"
-
-BOARD_NAME=$(cat "$BOARD_NAME_PATH")
 
 BRAIINS_BOARD_stm32mp157c_ii1_am2="braiins,stm32mp157c-ii1-am2"
 
@@ -14,3 +13,28 @@ PART_NAME_FIP="fip"
 
 NVMEM_WORD_SIZE=4
 NVMEM_OTP_MINER_HWID=63
+
+bos_major() {
+	cat "$BOS_MAJOR_PATH" 2>/dev/null
+	return 0
+}
+
+bos_mode() {
+	cat "$BOS_MODE_PATH" 2>/dev/null
+	return 0
+}
+
+bos_platform() {
+	cat "$BOS_PLATFORM_PATH" 2>/dev/null
+	return 0
+}
+
+bos_version() {
+	cat "$BOS_VERSION_PATH" 2>/dev/null
+	return 0
+}
+
+miner_hwid() {
+	cat "$MINER_HWID_PATH" 2>/dev/null
+	return 0
+}
