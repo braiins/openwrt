105f64a6557475477e5a998698b645605e1194f4
Libor Vašíček <libor.vasicek@braiins.cz>
2024-08-09 20:37:44 +0200

    stm32mp15_ii2: Create new Linux stm32mp15 sub-target ii2 #BOS-2631
    - clone 'target.mk' from 'target/linux/stm32mp15/ii1'

---
+++ target/linux/stm32mp15/ii2/target.mk
@@ -0,0 +1,15 @@
+# SPDX-License-Identifier: GPL-2.0-only
+#
+# Copyright (C) 2023  Braiins Systems s.r.o.
+
+include $(TOPDIR)/rules.mk
+
+BOARDNAME:=Braiins Control Board (stm32mp15_ii1)
+CPU_TYPE:=cortex-a7
+CPU_SUBTYPE:=neon-vfpv4
+
+BOARD_IFACE := am2
+
+SUBTARGET_COMPATIBLES := \
+	braiins,stm32mp157c-ii1-am2 \
+	braiins,stm32mp157c-ii1-am2_p03_a12
