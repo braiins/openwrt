b411ec004727560675e39a89bb9ac845257398d8
Martin Hrůza <martin.hruza@braiins.cz>
2024-11-07 23:29:25 +0100

    openwrt: Fix wifi configuration #BOS-2838
    - force_link=0 ensures that ip address and default route are unset in
      the system when link is lost. Without this and with static ip address
      on the ethernet, the default route to the ethernet is still present no
      matter if the cable isconnected or not which leads to non working wifi
      connection.

--- target/linux/stm32mp15/base-files/etc/uci-defaults/01-edit-network-cfg.sh
+++ target/linux/stm32mp15/base-files/etc/uci-defaults/01-edit-network-cfg.sh
@@ -13,6 +13,7 @@ case "$board" in
 		# See comments in /etc/board.d/02_network for details.
 		if uci -q get network.lan_temp && ! uci -q get network.lan; then
 			uci -q rename network.lan_temp='lan'
+			uci -q set network.lan.force_link='0'
 		fi
 
 		# Remove IPv6 config, we don't want it for now.
